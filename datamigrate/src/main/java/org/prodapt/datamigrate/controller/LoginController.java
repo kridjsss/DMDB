59faa207829f71ba92b735f4027cd78617f68d26
kridjsss <kridjsss@gmail.com>
2016-05-31 17:31:31 +0530

    adding the log4j and url connection

--- datamigrate/src/main/java/org/prodapt/datamigrate/controller/LoginController.java
+++ datamigrate/src/main/java/org/prodapt/datamigrate/controller/LoginController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,10 +17,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class LoginController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+    private Logger logger = Logger.getLogger(LoginController.class.getName());
+    
 	@RequestMapping(value = { "/", "/home" }, method = RequestMethod.GET)
     public String homePage(ModelMap model) {
         model.addAttribute("greeting", "Hi, Welcome to mysite. ");
+        logger.info("Accessing the home page.");
         return "home";
     }
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
---
+++ datamigrate/src/main/java/org/prodapt/datamigrate/utilities/URLConnection.java
@@ -0,0 +1,64 @@
+package org.prodapt.datamigrate.utilities;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.apache.log4j.Logger;
+
+public class URLConnection {
+	private static Logger logger = Logger.getLogger(URLConnection.class.getName());
+	
+	public static String establish(String url, String requestMethod, String json, String contentType){
+		BufferedReader in = null;
+		String res = null;
+		
+		try {
+			URL obj = new URL(url);
+			
+			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+			
+			//set request method
+			con.setRequestMethod(requestMethod);
+			
+			if("POST".equals(requestMethod)){
+				con.setDoOutput(true);
+				DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+				wr.writeBytes(json);
+				wr.flush();
+				wr.close();
+			}
+			
+			//add request header
+			con.setRequestProperty("Accept-Charset", "UTF-8");
+			con.setRequestProperty("Content-Type", contentType);
+			
+			int responseCode = con.getResponseCode();
+			logger.info("Response Code:"+responseCode);
+			
+			in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			
+			String inputLine;
+			StringBuilder response = new StringBuilder();
+			
+			while((inputLine = in.readLine())!=null){
+				response.append(inputLine);
+			}
+			in.close();
+			res = response.toString();
+			
+			logger.info("Response:"+response);
+			
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return res;
+	}
+
+}
