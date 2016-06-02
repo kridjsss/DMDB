a70c570b02391fc88595a5749afc4c4e20c37dce
kridjsss <kridjsss@gmail.com>
2016-06-02 17:13:52 +0530

    Integrating the velocity template engine

--- datamigrate/src/main/java/org/prodapt/datamigrate/controller/LoginController.java
+++ datamigrate/src/main/java/org/prodapt/datamigrate/controller/LoginController.java
@@ -1,9 +1,14 @@
 package org.prodapt.datamigrate.controller;
 
+import java.io.StringWriter;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
 import org.prodapt.datamigrate.utilities.MailHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -20,10 +25,19 @@ public class LoginController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	@Autowired
 	MailHandler sendMailBean;
-
+	@Autowired
+	VelocityEngine velocityEngine;
+	
 	@RequestMapping(value = { "/", "/home" }, method = RequestMethod.GET)
 	public String homePage(ModelMap model) {
 		model.addAttribute("greeting", "Hi, Welcome to mysite. ");
+		VelocityContext context = new VelocityContext();
+		context.put("userName", getPrincipal());
+		Template template = velocityEngine.getTemplate("templates/hello.vm");
+		StringWriter writer = new StringWriter();
+		template.merge(context, writer);
+//		System.out.println("template content:"+writer);
+		sendMailBean.sendExceptionMail("Welcome to Login",writer.toString());
 		return "home";
 	}
 
@@ -43,8 +57,12 @@ public class LoginController extends HttpServlet {
 						auth);
 			}
 		} catch (Exception e) {
-			sendMailBean.sendExceptionMail("Exception in Logout",
-					e.getMessage());
+			VelocityContext context = new VelocityContext();
+			context.put("userName", "krishna");
+			Template template = velocityEngine.getTemplate("templates/hello.vm");
+			StringWriter writer = new StringWriter();
+			template.merge(context, writer);
+			sendMailBean.sendExceptionMail(writer.toString(),e.getMessage());
 		}
 		return "redirect:/login?logout";
 	}
--- datamigrate/src/main/webapp/WEB-INF/views/modules.jsp
+++
@@ -1,2 +0,0 @@
-
-<div id="chart_div"></div>
